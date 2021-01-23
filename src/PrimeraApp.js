1f018f6f21302e91d59215073b4ae9f37e074ec6
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-01-23 10:40:55 -0500

    prop - types para agregar validaiones a los datos que entran en el componente

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 //import React, { Fragment } from 'react';
 
 const PrimeraApp = ({saludo = 'Hola Mundo'}) =>{
+
     return (
         <>
             <h1> { saludo } </h1>
             <p>asdasdasd</p>
         </>
     );
+}
+//para agregar validaciones a los campos que se envan desde otro modulo
+//se realiza importando PropTypes de 'prop-types'
+//se hace refrencia a la constante que va a ser exportada y 
+//dentro se agregan las validaciones de cada una varuiables de 
+//entrada, hay algunas, revise, investigue
+PrimeraApp.propTypes ={
+    saludo: PropTypes.string.isRequired
 }
 
 export default PrimeraApp;
--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ const divRoot = document.querySelector('#root');
 //para enviar parametros se envia despues del nombre de la 
 //etiqueta, en caso de que no se envie al moemno de recirbir sera undefned
 //por lo cual en el otro lado se podria poner un valor por default
-ReactDOM.render(<PrimeraApp saludo = "hola soy goku" />, divRoot)
+ReactDOM.render(<PrimeraApp  />, divRoot)
 
