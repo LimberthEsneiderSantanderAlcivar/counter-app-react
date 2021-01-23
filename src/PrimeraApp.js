77bb6046f3044c9e4d4b92d77c01df3a0c8531df
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-01-23 10:33:01 -0500

    props

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,18 +1,10 @@
 import React from 'react';
 //import React, { Fragment } from 'react';
 
-const PrimeraApp = () =>{
-    //los booleanos no se imprimen en principio
-    //si se pone un arreglo los concatena uno tras otr sin la coma [1,2,3] = 123
-    //los obejtos dan error al imprimir en principio solo con las llaves, para eso se debe utilizar un JSON.stringify
-    const saludo = {
-        nombre:'Tomas',
-        edad: 32
-    };
+const PrimeraApp = ({saludo = 'Hola Mundo'}) =>{
     return (
         <>
-            <h1> Hola { saludo.nombre } </h1>
-            <pre> { JSON.stringify( saludo, null, 3) } </pre>
+            <h1> { saludo } </h1>
             <p>asdasdasd</p>
         </>
     );
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './index.css'
 const divRoot = document.querySelector('#root');
 
 
-
-ReactDOM.render(<PrimeraApp/>, divRoot)
+//para enviar parametros se envia despues del nombre de la 
+//etiqueta, en caso de que no se envie al moemno de recirbir sera undefned
+//por lo cual en el otro lado se podria poner un valor por default
+ReactDOM.render(<PrimeraApp saludo = "hola soy goku" />, divRoot)
 
