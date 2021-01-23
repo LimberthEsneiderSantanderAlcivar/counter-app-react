8f43e3e26087051defa0e484c89f9fcbca5bc232
LimberthEsneiderSantanderAlcivar <esneider_98@outlook.com>
2021-01-23 09:49:29 -0500

    fragment y la etiqueta sin nombre que abarca los demas compoentes y es 'ignorada'

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,6 +1,19 @@
 import React from 'react';
+//import React, { Fragment } from 'react';
+
+//Fragmen es para que sea un div el dual react va a "ignorar"
+//se usa porque el return debe tener un elemento html padre principal
+//que abarque todos los demas compoenentes
+//otra manera de evitar eso es no importar ni susar fragment
+//sino solo poner las etiquetas sin ningun texto dentro como se 
+//ve en la etiqueta que encierra los componentes ahi abajito
 const PrimeraApp = () =>{
-    return <h1>Hola Mundo</h1>
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>asdasdasd</p>
+        </>
+    );
 }
 
 export default PrimeraApp;
